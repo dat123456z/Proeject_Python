aecc8520936d6bdc696ecb34cd73d13da1118a76
DavidDet <quocdat07022005@gmail.com>
2024-11-10 14:57:30 +0700

    fix loi vat

--- setup.py
+++ setup.py
@@ -19,8 +19,8 @@ def get_requirements(file_path:str)->List[str]:
 setup(
 name='PROJECT_PYTHON_MAIN',
 version='0.0.1',
-author='Hieu',
-author_email='bacdoan52@gmail.com',
+author='Dat',
+author_email='quocdat07022005@gmail.com',
 packages=find_packages(),
 install_requires=get_requirements('requirements.txt')
 
